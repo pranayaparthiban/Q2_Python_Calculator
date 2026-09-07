12be7a34228479b4342a6051bc33a78101f384c3
pranayaparthiban <pranayaparthi@gmail.com>
2026-09-08 03:34:51 +0530

    Add Python calculator

---
+++ calculator.py
@@ -0,0 +1,14 @@
+num1 = 20
+num2 = 10
+
+print("Number 1:", num1)
+print("Number 2:", num2)
+
+print("Addition:", num1 + num2)
+print("Subtraction:", num1 - num2)
+print("Multiplication:", num1 * num2)
+
+if num2 != 0:
+    print("Division:", num1 / num2)
+else:
+    print("Division: Cannot divide by zero")
